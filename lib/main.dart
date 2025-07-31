2b62805965a99f0c54d4e6aaec5eaeb85cc2f57a
BamsongE <soog2530@naver.com>
2025-08-01 08:43:31 +0900

    파이어베이스 옵션 경로 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:muse_mate/firebase_options.dart';
+import 'package:muse_mate/config/firebase_options.dart';
 import 'package:muse_mate/screen/login_screen.dart';
 
 void main() async {
